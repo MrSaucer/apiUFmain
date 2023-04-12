4648f0cec611b2a95ed4708b2a26a33269653acd
MrSaucer <108151584+MrSaucer@users.noreply.github.com>
2023-04-12 06:44:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const db = require('./db.json');//requisição do banco de dados externo.
+const db = require('./db.json');
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-
     res.json(db)
+});
+
+app.get(('/ac/nome'), (req, res) => {
+    res.json(db[0].nome)
 });
 
 app.listen(8080, () => {
     let data = new Date();
     console.log("servidor iniciado em: " + data);
-    console.log(db[2].nome);
 })
